28eb2f9bd72f52e3b625c3573bacb4086dad30c2
Hyperion dev <kwijwna@gmail.com>
2025-05-03 18:43:45 +0800

    Update hyperion.lua

--- hyperion.lua
+++ hyperion.lua
@@ -38,7 +38,8 @@ end
 
 noti("WARNING", "you need the ArkenStone.", 2)
 noti("WARNING", "this script isnt updated by loadstring.", 2)
-
+noti("join our discord.")
+setclipboard("https://discord.gg/AZ2FGeZHF8")
 
 -- vars here
 local chat = game:GetService("TextChatService").TextChannels.RBXSystem
